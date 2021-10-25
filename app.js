fa055934954dfeae7886844037fa96fe26145532
MrPleus <93022794+MrPleus@users.noreply.github.com>
2021-10-25 13:44:45 -0400

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const Burger = document.querySelector(".burger")
-const navlinks =document.querySelector(".navlinks")
-
-Burger.addEventListener("click", () => {
-     navlinks.classList.toggle("change")
-     Burger.classList.toggle("active")
-})
